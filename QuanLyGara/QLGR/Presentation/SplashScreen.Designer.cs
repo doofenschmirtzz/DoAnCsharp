8f76fc4084830bf23e31344534c1d65b9982fb86
doofenschmirtzz <33329300+doofenschmirtzz@users.noreply.github.com>
2018-01-14 16:16:28 +0700

    UPDATE1

---
+++ QuanLyGara/QLGR/Presentation/SplashScreen.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace QLGR.Presentation
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.ProgressTimer = new System.Windows.Forms.Timer(this.components);
+            this.progressBarX1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(235, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelX1
+            // 
+            this.labelX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.ForeColor = System.Drawing.Color.White;
+            this.labelX1.Location = new System.Drawing.Point(22, 264);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(644, 52);
+            this.labelX1.TabIndex = 1;
+            this.labelX1.Text = "PHẦN MỀM QUẢN LÝ GARA OTO";
+            // 
+            // labelX4
+            // 
+            this.labelX4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX4.ForeColor = System.Drawing.Color.White;
+            this.labelX4.Location = new System.Drawing.Point(580, 313);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(86, 16);
+            this.labelX4.TabIndex = 1;
+            this.labelX4.Text = "Version 1.0.1";
+            // 
+            // ProgressTimer
+            // 
+            this.ProgressTimer.Enabled = true;
+            this.ProgressTimer.Interval = 32;
+            this.ProgressTimer.Tick += new System.EventHandler(this.ProgressBar_Tick);
+            // 
+            // progressBarX1
+            // 
+            this.progressBarX1.Location = new System.Drawing.Point(51, 354);
+            this.progressBarX1.Name = "progressBarX1";
+            this.progressBarX1.Size = new System.Drawing.Size(580, 23);
+            this.progressBarX1.TabIndex = 2;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(687, 396);
+            this.Controls.Add(this.progressBarX1);
+            this.Controls.Add(this.labelX4);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private System.Windows.Forms.Timer ProgressTimer;
+        private System.Windows.Forms.ProgressBar progressBarX1;
+    }
+}
